eb10dd8e377a968a4080aeb60db367f130bdab10
Pscly <Pscly@outlook.com>
2021-08-08 16:12:35 +0800

    数据写入

---
+++ app01/apis/myfuncs.py
@@ -0,0 +1,17 @@
+
+
+def write_data(data):
+    """
+    写数据
+    :return:
+    """
+
+def write_file(data):
+    """
+    写文件
+    :param data:
+    :param file_name:
+    :return:
+    """
+    with open(f"data['who'].data", 'w') as f:
+        f.write(data)
