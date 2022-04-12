099ba6966d6c4fb47af3aed9fe3f240ab69f56c3
pscly <pscly@outlook.com>
2022-04-13 07:16:41 +0800

    去除刚才没有删除完毕的东西

--- flask_s/utils/send_email.py
+++ flask_s/utils/send_email.py
@@ -8,7 +8,6 @@ def send_email(body, email_data:dict={}):
     if not email_data:
         email_data = os.y.email_data
     yag = yagmail.SMTP(email_data.E_USER, email_data.E_PWD, host=email_data.E_HOST)
-    ip = get_ip()
     send_data = ['pscly@qq.com', '来自my_end,', body]
     
     try:
