97ac08325cf80c2652f7cd68893d6f2f28e41565
pscly <pscly@outlook.com>
2022-08-23 02:54:09 +0800

    添加了files2

---
+++ flask_s/app01/apis/files2/view_files.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, jsonify, current_app, g, send_file, render_template
+from app01 import myfuncs
+from addict import Dict
+from utils import send_email
+from datetime import datetime
+import time
+import os
+from entities import data_saves
+from utils import core
+
+service_name = 'files2'
+
+bp = Blueprint(service_name, __name__, url_prefix=f'/{service_name}')
+
+
+def get_size(fobj):
+    if fobj.content_length:
+        return fobj.content_length
+
+    try:
+        # 这里是使用的文件io指针
+        pos = fobj.tell()
+        fobj.seek(0, 2)
+        size = fobj.tell()
+        fobj.seek(pos)
+        return size
+    except (AttributeError, IOError):
+        pass
+    return 0  # assume small enough
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    datas = myfuncs.get_datas(request)
+    data_saves.save_data(datas, 1, service_name)
+    files = core.get_files(os.y.up_files_path)
+    return render_template('down.html', files=files)
+
+
+@bp.route('/<string:file_name>', methods=('GET',))
+def down(file_name):
+    datas = myfuncs.get_datas(request)
+    data_saves.save_data(datas, 1, f'{service_name}/down')
+    x = os.path.join(os.y.up_files_path, file_name)
+    if os.path.isfile(x):
+        return send_file(x)
+    return jsonify({'msg': '文件不存在'})
+
+
+@bp.route(rule='/up', methods=('GET', 'POST'))
+def up_file():
+    datas = myfuncs.get_datas(request)
+    data_saves.save_data(datas, 1, f'{service_name}/up')
+    if request.method == "GET":
+        date1 = time.strftime("%d%H")
+        if os.y.config.get('UP_FILE') or request.args.get('y') == date1:
+            return render_template('up_file.html')
+        return jsonify({'msg': '请求错误，此页面暂时不允许访问', 'y': request.args.get('y')})
+
+    if request.method == 'POST':
+        file = request.files.get('file')
+        # 如果文件大小超过300mb, 则返回错误
+
+        file_name = request.form.get('file_name') or file.filename
+        myfuncs.save_file(request.files.get('file'),
+                          os.path.join(os.y.up_files_path, file_name))
+        return render_template('down_ok.html')
--- flask_s/app01/route.py
+++ flask_s/app01/route.py
@@ -2,6 +2,7 @@ from app01.apis.geng import bp as geng_bp
 from app01.apis.end1.view_end1 import bp as end1_bp
 from app01.apis.email.view_email import bp as emi_bp
 from app01.apis.files.view_files import bp as files_bp
+from app01.apis.files2.view_files import bp as files2_bp
 from app01.apis.rum.view_rum import bp as rum_bp
 from app01.apis.texts.view_text import bp as texts_bp
 from app01.apis.datas.view_data import bp as datas_bp
@@ -12,6 +13,7 @@ routers = [
     end1_bp,        # 后端1的路由
     emi_bp,         # 邮件路由
     files_bp,       # 文件共享路由
+    files2_bp,       # 文件共享路由
     rum_bp,         # rum 相关
     texts_bp,         # rum 相关
     datas_bp,         # rum 相关
