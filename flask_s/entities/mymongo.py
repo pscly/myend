d17d832e2b7cab1edb0df371d31b20e25d1fe541
pscly <pscly@outlook.com>
2023-03-10 00:25:16 +0800

    test1

--- flask_s/entities/mymongo.py
+++ flask_s/entities/mymongo.py
@@ -3,19 +3,37 @@ import os
 
 
 class MyMongo1():
+    __instance = None
 
-    def __init__(self, table_name=None):
-        ip = os.y.config.get('mongodb_ip')
-        port = os.y.config.get('mongodb_port')
-        self.client = MongoClient(ip, port)
-        # self.client = MongoClient('127.0.0.1', 27017)
+    def __new__(cls, ip='127.0.0.1', port=27017, username=None, password=None, db_name='yend1', table_name='userinfo'):
+        """
+        
+        原本就光一个table_name就可以了 但是为了后续扩展性方便 我就加了个db_name 
+        """
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.ip = ip
+            cls.__instance.port = port
+            cls.__instance.username = username
+            cls.__instance.password = password
+            cls.__instance.db_name = db_name
+            cls.__instance.table_name = table_name or 'userinfo'
+            cls.__instance.connect()
+        return cls.__instance
 
-        self.db = self.client['yend1']  # 等同于：client.db1
-        if table_name:
-            self.table_user = self.db[table_name]
-        else:
-            self.table_user = self.db['userinfo']  # 等同于：db.user
-
+    def connect(self):
+        try:
+            client = MongoClient(host=self.ip, port=self.port,
+                                username=self.username, password=self.password, authSource=self.db_name,
+                                authMechanism='SCRAM-SHA-256')
+            self.db = client[self.db_name]
+            self.table_user = self.db[self.table_name]
+        except Exception as e:
+            print(f"Failed to connect to MongoDB: {e}")
+    def disconnect(self):
+        self.client.close()
+        
+    
     def save(self, data):
         """
         data 可以是一个dict or list
@@ -67,11 +85,18 @@ class MyMongo1():
 
 
 if __name__ == '__main__':
-    table = 'test1'
-    x = MyMongo1(table).find({"_id": "202040030805"})
-    print(x)
-    MyMongo1(table).save(
-        [{"_id": "111", "名字112345": "qeqw1re1332"},
-         {"_id": "222", "名字112345": "qeqw1re1332"}])
-    # 更新
-    # MyMongo1(table).update({"_id": "202040030805"}, {" 字112345": "11xxx"})
+    # table = 'test1'
+    # # x = MyMongo1(table).find({"_id": "202040030805"})
+    # print(x)
+    # x = MyMongo1()
+    # MyMongo1(table).save(
+    #     [{"_id": "111", "名字112345": "qeqw1re1332"},
+    #      {"_id": "222", "名字112345": "qeqw1re1332"}])
+    # # 更新
+    # # MyMongo1(table).update({"_id": "202040030805"}, {" 字112345": "11xxx"})
+
+    # 创建 MyMongo1 实例对象
+    mongo = MyMongo1(ip='192.168.2.2', port=27018, username='root', password='y1C;r;K:27ZO-PQnb>X3+5=f0@W9uePIJ',db_name='a1', table_name='t1')
+    # 查询 asd=123 的文档
+    result = mongo.table_user.find_one({'asd': 123})
+    print(result)
