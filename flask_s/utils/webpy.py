50013a2122cd232d58ecb4a2fa339dfe0562d9fe
pscly <pscly@outlook.com>
2023-07-17 15:25:12 +0800

    文件上传逻辑更新1

--- flask_s/utils/webpy.py
+++ flask_s/utils/webpy.py
@@ -7,6 +7,7 @@ class WebFunc:
         self.app = app
         app.jinja_env.filters["islogin1"] = self.islogin1
         app.jinja_env.filters["get_y1"] = self.get_y1
+        app.jinja_env.filters["in_str"] = self.in_str
 
     def islogin1(self, request1):
         if current_user.is_authenticated:
@@ -15,3 +16,9 @@ class WebFunc:
 
     def get_y1(self, request1):
         return time.strftime("%H%m%d")
+
+    def in_str(self, str1):
+        str1, str2 = str1[0], str1[1]
+        if str1 in str2:
+            return True
+        return False
