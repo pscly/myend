c7ab9126a66edcba68a1d53f04a5561fcbc91f4d
pscly <pscly@outlook.com>
2024-06-06 11:18:45 +0800

    合并了xt的东西

---
+++ flask_s/entities/to_db.py
@@ -0,0 +1,9 @@
+
+"""
+
+
+"""
+
+from entities.data_saves import save_data
+from entities.mymongo import MyMongo1
+
---
+++ flask_s/utils/login1.py
@@ -0,0 +1,38 @@
+from flask_login import LoginManager, UserMixin
+import os
+from addict import Dict
+from entities.mymongo import MyMongo1
+
+
+def get_all_users():
+    mo = MyMongo1("users")
+    users1 = mo.find_all()
+    # users1 = Dict(mo.find({"user_type": "all_user"}) or {"user_type": "all_user", "all_users": {}})
+    # 将 user1 : [{"name": "pscly", "pwd": "123"},{"name": "bbb", "pwd": "1234"}]
+    # 转化为 users:  {"pscly": {"name": "pscly", "pwd": "123"}} ...
+    users = {}
+    for user in users1:
+        if user.get("name"):
+            users[user.get("name")] = Dict(user)
+    return users
+
+
+def get_one_user(name):
+    mo = MyMongo1("users")
+    users = Dict(mo.find({"name": name}))
+    return users
+
+
+def save_all_users():
+    mo = MyMongo1("users")
+    mo.save(os.y.users1)
+
+
+def save_one_user(userdata: dict):
+    mo = MyMongo1("users")
+    mo.save(userdata)
+
+
+class User(UserMixin):
+    def __init__(self):
+        pass
